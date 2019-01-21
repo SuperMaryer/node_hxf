cb20e6326d7f614d28c1d00d96c2b93557ec934e
SuperMaryer <1012073997.com>
2019-01-21 12:05:09 +0800

    3登录页面完成

--- src/app.js
+++ src/app.js
@@ -1,31 +1,52 @@
-//导包
+//1. 导包
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+var session = require('express-session')
 
-//创建app
+//2.创建app
 const app = express()
 
-//处理请求  测试
+
+//body-parser的两句话
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+// Use the session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 600000
+    }
+}))
+
+//测试
 // app.get('/',(err,res)=>{
-//     res.send('hello world!')
+//     res.send('hello world')
 // })
 
 
-//设置静态资源路径, 所有的第三方包, 最好都写在路由对象导入之前
-app.use(express.static(path.join(__dirname,'public')))
+//设置静态资源根目录
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 //导入路由对象
-const accountRouter = require(path.join(__dirname,'./routers/accountRouter.js'))
-app.use('/account',accountRouter)
+const accountRouter = require(path.join(__dirname, './routers/accountRouter.js'))
+app.use('/account', accountRouter)
+
+
 
 
 //启动
-app.listen(3000,'127.0.0.1',err=>{
-    if(err){
+app.listen(3000, '127.0.0.1', err => {
+    if (err) {
         console.log(err);
     }
-    console.log('start ok');
+    console.log('start ok!');
 })
-
-
--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,16 +1,158 @@
+const path = require("path");
 
-/**
- * module.exports = {
- *  getRegisterPage:箭头函数
- * }
- * 导出的一个方法，该方法获取注册页面
- */
-//导包
- const path = require('path')
+//数据库使用: 导入数据库
+const MongoClient = require("mongodb").MongoClient;
+//导入获取验证码图片的第三方包
+const captchapng = require('captchapng')
 
- //处理请求
-exports.getRegisterPage = (req,res)=>{
-    // res.send('我是注册页')
-    //res.sendFile() 是对fs.readFile 和res.send()的封装
-    res.sendFile(path.join(__dirname,'../public/views/register.html'))
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name  数据库名称
+const dbName = 'szhmqd27';
+
+
+
+//导出一个方法, 该方法获取注册页面
+module.exports.getRegisterPage = (req, res) => {
+    // res.sendFile() 内部就是对 fs.readFile 的封装
+    res.sendFile(path.join(__dirname, '../public/views/register.html'))
+}
+
+//导出一个方法, 完成注册
+exports.register = (req, res) => {
+
+    //1.设置默认返回数据
+    const result = {
+        status: 0,
+        message: '成功'
+    }
+    //通过post请求方式获取参数 body-parser   
+    const {
+        username
+    } = req.body
+
+    //判断, 用户名存在: 提示用户, 用户名不存在: 插入一条, 注册成功  操作数据库, mongodb
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, client) {
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        // Get the documents collection 链接到要操作的集合
+        const collection = db.collection('accountInfo');
+
+        //查询. 判断是否已被注册  没有查到就会返回null
+        collection.findOne({
+            username
+        }, (err, result1) => {
+            if (result1) {
+                //查询到有数据
+                result.status = 1,
+                    result.message = "用户名已存在"
+
+                //关闭数据库
+                client.close();
+                //返回数据
+                res.json(result)
+            } else {
+                //不存在, 新增一条 result2有值: 新增成功, 返回null:失败
+                collection.insertOne(req.body, (err, result2) => {
+                    if (!result2) {
+                        //失败
+                        result.status = 2,
+                            result.message = "注册失败"
+                    }
+                    //关闭数据库
+                    client.close();
+                    //返回数据
+                    res.json(result)
+                })
+            }
+
+        })
+    })
+}
+
+//获取登录页面
+exports.getLoginPage = (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/login.html'))
+}
+
+//获取验证码图片
+exports.getVcodeImage = (req, res) => {
+
+    const vcode = parseInt(Math.random() * 9000 + 1000)
+    //把vcode保存在session中去
+    console.log(vcode);
+    req.session.vcode = vcode
+    var p = new captchapng(80, 30, vcode); // width,height,numeric captcha
+    p.color(0, 0, 0, 0); // First color: background (red, green, blue, alpha)
+    p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
+
+    var img = p.getBase64();
+    var imgbase64 = new Buffer(img, 'base64');
+    res.writeHead(200, {
+        'Content-Type': 'image/png'
+    });
+    res.end(imgbase64);
+}
+
+// 导出登录的方法
+exports.login = (req, res) => {
+    //定义默认返回json数据
+    const result = {
+        status: 0,
+        message: "登录成功"
+    }
+
+    // 把浏览器传递过来的验证码 和 req.session.vcode 中的验证码对比
+    const {
+        vcode,
+        password,
+        username
+    } = req.body
+
+    if (vcode != req.session.vcode) {
+        //表示验证码正确, 链接数据库, 查询是否有这样的账号和密码的数据
+        console.log('111111');
+        result.status = 1,
+            result.message = "验证码失败"
+        res.json(result)
+        return
+    }
+
+    //来到这里说明验证码正确, 判断账号密码是否正确
+    //查询一个数据 , 没有数据返回null
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, client) {
+
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        // Get the documents collection //链接到要操作的集合
+        const collection = db.collection('accountInfo');
+
+        //查询一个
+        collection.findOne({
+            username,
+            password
+        }, (err, result1) => {
+            console.log(result1);
+
+            if (!result1) {
+                //没有查到: 登录失败
+                result.status = 2
+                result.message = '账号或者密码错误'
+            }
+            //关闭数据库
+            client.close();
+            //返回数据
+            res.json(result);
+        })
+
+    });
+
 }
